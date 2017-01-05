9dd29836cfd1d137084f8688915a563c92533271
Andrew Piper <piper.andrew@spcollege.edu>
2017-01-05 14:45:39 -0500

    Added a collection query class that takes the query args.

---
+++ src/Threax.AspNetCore.Halcyon.Ext/CollectionQuery.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Threax.AspNetCore.Halcyon.Ext
+{
+    /// <summary>
+    /// An interface to get the collection args from a query object.
+    /// </summary>
+    public interface ICollectionQuery
+    {
+        /// <summary>
+        /// The number of pages (item number = Offset * Limit) into the collection to query.
+        /// </summary>
+        int Offset { get; set; }
+
+        /// <summary>
+        /// The limit of the number of items to return.
+        /// </summary>
+        int Limit { get; set; }
+    }
+
+    /// <summary>
+    /// Default implementation of ICollectionQuery.
+    /// </summary>
+    public class CollectionQuery
+    {
+        /// <summary>
+        /// The number of pages (item number = Offset * Limit) into the collection to query.
+        /// </summary>
+        public int Offset { get; set; }
+
+        /// <summary>
+        /// The limit of the number of items to return.
+        /// </summary>
+        public int Limit { get; set; }
+    }
+}
