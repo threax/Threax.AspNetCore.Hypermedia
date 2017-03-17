5d39d6022b755e7306c0e2967d9a01e8db7bd489
Andrew Piper <piper.andrew@spcollege.edu>
2017-03-17 08:53:49 -0400

    Added a class for collections with search but no pages.

---
+++ src/Threax.AspNetCore.Halcyon.Ext/SearchCollectionView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Threax.AspNetCore.Halcyon.Ext
+{
+    public class SearchCollectionView<T> : CollectionView<T>, IQueryStringProvider
+    {
+        public SearchCollectionView(SearchQuery query, IEnumerable<T> items)
+            :base(items)
+        {
+        }
+
+        public string AddQuery(string rel, string url)
+        {
+            if (rel == HalSelfActionLinkAttribute.SelfRelName && Term != null)
+            {
+                url = url.AppendQueryString($"term={Term}");
+            }
+            return url;
+        }
+
+        public String Term { get; set; }
+    }
+}
