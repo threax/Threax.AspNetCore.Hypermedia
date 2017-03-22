f7cfd1460ba6b398eeb708e9b2994d9a59348fca
Andrew Piper <piper.andrew@spcollege.edu>
2017-03-22 13:44:08 -0400

    Changed EnumValueProvider to LabelValuePairProvider.

---
+++ src/Threax.AspNetCore.Halcyon.Ext/ValueProviders/LabelValuePair.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Threax.AspNetCore.Halcyon.Ext.ValueProviders
+{
+    public class LabelValuePair
+    {
+        public LabelValuePair()
+        {
+
+        }
+
+        public LabelValuePair(String label, String value)
+        {
+            this.Label = label;
+            this.Value = value;
+        }
+
+        public String Label { get; set; }
+
+        public String Value { get; set; }
+    }
+}
---
+++ src/Threax.AspNetCore.Halcyon.Ext/ValueProviders/LabelValuePairProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NJsonSchema;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Threax.AspNetCore.Halcyon.Ext.ValueProviders
+{
+    /// <summary>
+    /// Helper base class to create extension data with key value pairs
+    /// that go into an "x-values" attribute by default.
+    /// </summary>
+    public abstract class LabelValuePairProvider : IValueProvider
+    {
+        public LabelValuePairProvider()
+        {
+            
+        }
+
+        public void AddExtensions(JsonProperty schemaProp, ValueProviderArgs args)
+        {
+            var name = "x-values";
+            if(args.ValueProviderAttr.PropertyName != null)
+            {
+                name = args.ValueProviderAttr.PropertyName;
+            }
+
+            var sources = GetSources();
+
+            if (schemaProp.ExtensionData == null)
+            {
+                schemaProp.ExtensionData = new Dictionary<String, Object>();
+            }
+
+            schemaProp.ExtensionData.Add(name, sources);
+        }
+
+        protected abstract IEnumerable<LabelValuePair> GetSources();
+    }
+}
