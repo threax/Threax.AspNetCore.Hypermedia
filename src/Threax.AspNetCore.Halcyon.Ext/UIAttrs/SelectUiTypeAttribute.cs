8d4dc0809f1557863e4981b5710f4fc1a26c1c00
Andrew Piper <piper.andrew@spcollege.edu>
2017-03-22 13:49:04 -0400

    Added an attribute to customize the ui type.

---
+++ src/Threax.AspNetCore.Halcyon.Ext/UIAttrs/SelectUiTypeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Threax.AspNetCore.Halcyon.Ext.UIAttrs
+{
+    public class SelectUiTypeAttribute : UiTypeAttribute
+    {
+        public SelectUiTypeAttribute() : base("select")
+        {
+        }
+    }
+}
---
+++ src/Threax.AspNetCore.Halcyon.Ext/UIAttrs/UiTypeAttribute.cs
@@ -0,0 +1,15 @@
+﻿using NJsonSchema.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Threax.AspNetCore.Halcyon.Ext.UIAttrs
+{
+    public class UiTypeAttribute : JsonSchemaExtensionDataAttribute
+    {
+        public UiTypeAttribute(object value) : base("x-ui-type", value)
+        {
+        }
+    }
+}
