947ce68417a947ebf255a109c993329073064a92
Andrew Piper <piper.andrew@spcollege.edu>
2017-05-05 13:13:35 -0400

    Added a crudrel for list and a UiSearchAttribute to mark a property as part of a search ui.

---
+++ src/Threax.AspNetCore.Halcyon.Ext/UIAttrs/UiSearchAttribute.cs
@@ -0,0 +1,20 @@
+﻿using NJsonSchema.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Threax.AspNetCore.Halcyon.Ext.UIAttrs
+{
+    /// <summary>
+    /// Use this attribute to specify that the property should be shown on auto generated
+    /// search uis.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class UiSearchAttribute : JsonSchemaExtensionDataAttribute
+    {
+        public UiSearchAttribute() : base("x-ui-search", true)
+        {
+        }
+    }
+}
