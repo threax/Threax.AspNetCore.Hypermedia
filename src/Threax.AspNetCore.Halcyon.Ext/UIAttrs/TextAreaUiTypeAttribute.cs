b037771c3430ff60580e3a134a7ae33088c1ab56
Andrew Piper <piper.andrew@spcollege.edu>
2017-08-03 11:38:06 -0400

    Added a ui type attribute for text areas.

---
+++ src/Threax.AspNetCore.Halcyon.Ext/UIAttrs/TextAreaUiTypeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Threax.AspNetCore.Halcyon.Ext.UIAttrs
+{
+    /// <summary>
+    /// Use this to change the ui type of a property to a hidden. This way you can include
+    /// a piece of data in an editing object without displaying it to the user.
+    /// </summary>
+    public class TextAreaUiTypeAttribute : UiTypeAttribute
+    {
+        public TextAreaUiTypeAttribute() : base("textarea")
+        {
+        }
+    }
+}
