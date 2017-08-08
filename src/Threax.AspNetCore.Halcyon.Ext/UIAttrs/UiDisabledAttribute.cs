8a32377e16b277b2647017d2ea9db5ddccd5e351
Andrew Piper <piper.andrew@spcollege.edu>
2017-08-08 14:10:01 -0400

    Added a ui disabled attribute.

---
+++ src/Threax.AspNetCore.Halcyon.Ext/UIAttrs/UiDisabledAttribute.cs
@@ -0,0 +1,21 @@
+﻿using NJsonSchema.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Threax.AspNetCore.Halcyon.Ext.UIAttrs
+{
+    /// <summary>
+    /// This attribute sets a field to disabled by setting its x-ui-disabled attribute.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class UiDisabledAttribute : JsonSchemaExtensionDataAttribute
+    {
+        public const String ExtensionName = "x-ui-disabled";
+
+        public UiDisabledAttribute(bool disabled = true) : base(ExtensionName, disabled)
+        {
+        }
+    }
+}
