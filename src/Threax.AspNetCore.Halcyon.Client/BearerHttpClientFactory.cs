6163485eb75b95feebc7734f21def6390cc28e0d
Andrew Piper <piper.andrew@spcollege.edu>
2017-08-23 11:21:21 -0400

    Added a BearerHttpClientFactory class that creates requests with bearer tokens.

---
+++ src/Threax.AspNetCore.Halcyon.Client/BearerHttpClientFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Threax.AspNetCore.Halcyon.Client
+{
+    public class BearerHttpClientFactory : IHttpClientFactory
+    {
+        private String accessToken;
+        private IHttpClientFactory next;
+
+        public BearerHttpClientFactory(String accessToken, IHttpClientFactory next)
+        {
+            this.accessToken = accessToken;
+            this.next = next;
+        }
+
+        public HttpClient GetClient()
+        {
+            return next.GetClient();
+        }
+
+        public HttpRequestMessage GetRequestMessage()
+        {
+            var req = next.GetRequestMessage();
+            req.Headers.TryAddWithoutValidation(HeaderName, accessToken);
+            return req;
+        }
+
+        public String HeaderName { get; set; } = "bearer";
+    }
+}
