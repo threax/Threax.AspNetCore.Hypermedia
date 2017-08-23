07eb0eb3418c1b8cce2b46f295c7c890a58e1c63
Andrew Piper <piper.andrew@spcollege.edu>
2017-08-23 11:38:37 -0400

    Added a way to use dependency injection for the halcyon client.

---
+++ src/Threax.AspNetCore.Halcyon.Client/HalcyonClientDiExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Threax.AspNetCore.Halcyon.Client;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class HalcyonClientDiExtensions
+    {
+        /// <summary>
+        /// Add the halcyon client services, this will register IHttpClientFactory as a singleton. 
+        /// You can decorate it as needed by supplying a configureOptions callback.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configureOptions"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddHalcyonClient(this IServiceCollection services, Action<HalcyonClientOptions> configureOptions = null)
+        {
+            var options = new HalcyonClientOptions();
+            configureOptions?.Invoke(options);
+
+            services.TryAddSingleton<IHttpClientFactory>(s =>
+            {
+                IHttpClientFactory factory = new DefaultHttpClientFactory();
+                if (options.DecorateClientFactory != null)
+                {
+                    factory = options.DecorateClientFactory(factory);
+                }
+                return factory;
+            });
+
+            return services;
+        }
+    }
+}
---
+++ src/Threax.AspNetCore.Halcyon.Client/HalcyonClientOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threax.AspNetCore.Halcyon.Client
+{
+    public class HalcyonClientOptions
+    {
+        /// <summary>
+        /// This is called when the IHttpClientFactory is being created. You can decorate the instance
+        /// with additional IHttpClientFactory instances during this callback. Be sure to return your
+        /// outer decorator.
+        /// If this is null, the DefaultHttpClientFactory will be used directly.
+        /// </summary>
+        public Func<IHttpClientFactory, IHttpClientFactory> DecorateClientFactory { get; set; } = null;
+    }
+}
