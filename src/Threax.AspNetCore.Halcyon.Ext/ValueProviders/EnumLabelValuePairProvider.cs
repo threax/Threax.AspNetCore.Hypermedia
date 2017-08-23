5077c5ef7f02c96632ff26e02a9f389d88c060bc
Andrew Piper <piper.andrew@spcollege.edu>
2017-08-23 13:35:12 -0400

    Made the EnumLabelValuePairProvider set the enumeration and enumeration names properties instead of x-values, this makes it discoverable by the c# client writer.

--- src/Threax.AspNetCore.Halcyon.Ext/ValueProviders/EnumLabelValuePairProvider.cs
+++ src/Threax.AspNetCore.Halcyon.Ext/ValueProviders/EnumLabelValuePairProvider.cs
@@ -4,10 +4,12 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
+using NJsonSchema;
 
 namespace Threax.AspNetCore.Halcyon.Ext.ValueProviders
 {
-    public class EnumLabelValuePairProvider : LabelValuePairProviderSync
+    public class EnumLabelValuePairProvider : IValueProvider
     {
         Type enumType;
         bool nullable;
@@ -31,7 +33,25 @@ namespace Threax.AspNetCore.Halcyon.Ext.ValueProviders
             this.propertyInfo = propertyInfo;
         }
 
-        protected override IEnumerable<ILabelValuePair> GetSourcesSync()
+        public Task AddExtensions(JsonProperty schemaProp, ValueProviderArgs args)
+        {
+            var sources = GetSourcesSync();
+
+            foreach(var source in sources)
+            {
+                schemaProp.EnumerationNames.Add(source.Label);
+                schemaProp.Enumeration.Add(source.Value);
+            }
+
+            if (schemaProp.ExtensionData == null)
+            {
+                schemaProp.ExtensionData = new Dictionary<String, Object>();
+            }
+
+            return Task.FromResult(0);
+        }
+
+        protected virtual IEnumerable<LabelValuePair> GetSourcesSync()
         {
             if (nullable)
             {
