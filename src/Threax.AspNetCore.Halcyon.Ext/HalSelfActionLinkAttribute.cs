8b48962d4052cbdede3ba58f4e0889d7d75098bd
Andrew Piper <piper.andrew@spcollege.edu>
2017-09-21 14:32:50 -0400

    Converted entry point controller to use new style HalActionLinks also made self link able to be expressed the new way.

--- src/Threax.AspNetCore.Halcyon.Ext/HalSelfActionLinkAttribute.cs
+++ src/Threax.AspNetCore.Halcyon.Ext/HalSelfActionLinkAttribute.cs
@@ -22,10 +22,25 @@ namespace Threax.AspNetCore.Halcyon.Ext
         /// These links will include thier request query and have no docs
         /// by default. They should also only be placed on get rels.
         /// </summary>
-        /// <param name="rel"></param>
-        /// <param name="controllerType"></param>
-        /// <param name="routeArgs"></param>
-        /// <param name="title"></param>
+        /// <param name="controllerType">The type of the controller.</param>
+        /// <param name="funcName">The name of the function on the controller to lookup.</param>
+        /// <param name="routeArgs">Any additional route args.</param>
+        /// <param name="title">The title.</param>
+        public HalSelfActionLinkAttribute(Type controllerType, String funcName, String[] routeArgs = null, string title = null)
+            : base(SelfRelName, controllerType, funcName, routeArgs, title)
+        {
+            HasDocs = false;
+        }
+
+        /// <summary>
+        /// Create a new link based on a controller and a function.
+        /// These links will include thier request query and have no docs
+        /// by default. They should also only be placed on get rels.
+        /// </summary>
+        /// <param name="rel">The name of the rel to lookup.</param>
+        /// <param name="controllerType">The type of the controller.</param>
+        /// <param name="routeArgs">Any additional route args.</param>
+        /// <param name="title">The title.</param>
         public HalSelfActionLinkAttribute(string rel, Type controllerType, String[] routeArgs = null, string title = null)
             :base(SelfRelName, rel, controllerType, routeArgs, title)
         {
