2443acf6c9df207ba4a3904ce1861dae025cbce1
Andrew Piper <piper.andrew@spcollege.edu>
2017-11-13 13:56:25 -0500

    Added a class for a model changing's args.

---
+++ src/Threax.AspNetCore.Models/ModelChangedArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threax.AspNetCore.Models
+{
+    /// <summary>
+    /// A class that can be used to alert that a model is changing.
+    /// </summary>
+    /// <typeparam name="TModel">The type of the model.</typeparam>
+    public class ModelChangedArgs<TModel>
+    {
+        /// <summary>
+        /// The old value.
+        /// </summary>
+        public TModel Old { get; set; }
+
+        /// <summary>
+        /// The new value.
+        /// </summary>
+        public TModel New { get; set; }
+    }
+}
