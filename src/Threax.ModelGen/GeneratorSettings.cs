45121258996639f127ca6b83c34c4e3611cd7683
Andrew Piper <andrew@threax.com>
2017-11-29 19:05:42 -0500

    Removed checks for nullable, not going to be needed.

--- src/Threax.ModelGen/GeneratorSettings.cs
+++ src/Threax.ModelGen/GeneratorSettings.cs
@@ -11,9 +11,6 @@ namespace Threax.ModelGen
 {
     class GeneratorSettings
     {
-        //public const String OriginalTypeExtensionName = "x-original-type";
-        public const String NullableTypeExtensionName = "x-nullable-type";
-
         public void Configure()
         {
             if (Path.GetExtension(Source) != ".json")
@@ -39,44 +36,10 @@ namespace Threax.ModelGen
                     var prop = type.GetProperty(schemaProp.Name);
                     if(prop != null)
                     {
-                        if (schemaProp.ExtensionData == null) //Make sure this exists
-                        {
-                            schemaProp.ExtensionData = new Dictionary<String, Object>();
-                        }
-
                         var propType = prop.PropertyType;
-                        var propTypeInfo = propType.GetTypeInfo();
-                        var isEnum = propTypeInfo.IsEnum;
-                        var isNullable = false;
                         schemaProp.Type = JsonObjectType.Object;
                         schemaProp.Format = propType.Name;
-                        //schemaProp.ExtensionData.Add(OriginalTypeExtensionName, propType.Name);
 
-                        //Check nullable
-                        //Check to see if the value can be null, value types are considered null if they are nullables, 
-                        //reference types are considered nullable if they are not marked with a Required attribute.
-                        if (propTypeInfo.IsGenericType && propTypeInfo.GetGenericTypeDefinition() == typeof(Nullable<>)) //See if the type is a Nullable<T>, this will handle value types
-                        {
-                            //If this is nullable get the generic arg and use that as the prop type
-                            propType = propTypeInfo.GetGenericArguments()[0];
-                            propTypeInfo = propType.GetTypeInfo();
-                            isEnum = propTypeInfo.IsEnum;
-                            isNullable = true;
-                        }
-                        else if (!isEnum) //Skip enum types, those should be nullable, otherwise they are required.
-                        {
-                            //Check for the Required attribute, if it is not there consider the property to be nullable
-                            var requiredAttr = prop.GetCustomAttributes().FirstOrDefault(i => i.GetType() == typeof(RequiredAttribute)) as RequiredAttribute;
-                            if (requiredAttr == null)
-                            {
-                                isNullable = true;
-                            }
-                        }
-
-                        if (isNullable)
-                        {
-                            schemaProp.ExtensionData.Add(NullableTypeExtensionName, propType.Name);
-                        }
                         //If we allow collections check the collection type
                         if (typeof(System.Collections.IEnumerable).IsAssignableFrom(propType) && propType.GenericTypeArguments.Length > 0)
                         {
