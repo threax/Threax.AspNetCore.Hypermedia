856cd68a0f5c034364ce9b09b0f26a2093d79901
Andrew Piper <piper.andrew@spcollege.edu>
2017-12-14 10:50:44 -0500

    Regenerated dev app migrations.

--- src/Threax.AspNetCore.Models/UiAttrs/TextAreaUiTypeAttribute.cs
+++ src/Threax.AspNetCore.Models/UiAttrs/TextAreaUiTypeAttribute.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 namespace Threax.AspNetCore.Models
 {
     /// <summary>
-    /// Use this to change the ui type of a property to a hidden. This way you can include
-    /// a piece of data in an editing object without displaying it to the user.
+    /// Use this to change the ui type of a property to a textarea.
     /// </summary>
     public class TextAreaUiTypeAttribute : UiTypeAttribute
     {
