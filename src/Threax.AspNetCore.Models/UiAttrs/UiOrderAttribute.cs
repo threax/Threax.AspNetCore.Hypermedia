de6be7a040809096ae07e7493e55dc73fe984849
Andrew Piper <piper.andrew@spcollege.edu>
2017-12-14 10:12:02 -0500

    Moved ui order extension method into the attribute file.

--- src/Threax.AspNetCore.Models/UiAttrs/UiOrderAttribute.cs
+++ src/Threax.AspNetCore.Models/UiAttrs/UiOrderAttribute.cs
@@ -1,4 +1,5 @@
-﻿using NJsonSchema.Annotations;
+﻿using NJsonSchema;
+using NJsonSchema.Annotations;
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -21,6 +22,37 @@ namespace Threax.AspNetCore.Models
         /// <param name="offset">An offset to add to the calculated order value. Useful to try to keep order between multiple classes.</param>
         public UiOrderAttribute(int offset = 0, [CallerLineNumber] int order = 0) : base("x-ui-order", order + offset)
         {
+        }
+    }
+
+    public static class OrderAttributeJsonSchemaExtensions
+    {
+        /// <summary>
+        /// Makes use of the x-ui-order extension provided by the UiOrder class in the halcyon extensions, use that to specify order.
+        /// </summary>
+        internal const String Name = "x-ui-order";
+
+        /// <summary>
+        /// Get the order of the property. This will return null if there is no order.
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <returns></returns>
+        public static int? GetOrder(this JsonProperty prop)
+        {
+            Object val = null;
+            prop.ExtensionData?.TryGetValue(Name, out val);
+            return (int?)val;
+        }
+
+        /// <summary>
+        /// Set the order of this property.
+        /// </summary>
+        /// <param name="prop"></param>
+        /// <param name="order"></param>
+        public static void SetOrder(this JsonProperty prop, int order)
+        {
+            prop.ExtensionData = prop.ExtensionData ?? new Dictionary<String, Object>();
+            prop.ExtensionData[Name] = order;
         }
     }
 }
