7af6e2018c2d3acb62dc733dbe0a139a6aa2b163
Andrew Piper <piper.andrew@spcollege.edu>
2018-01-24 10:03:26 -0500

    Made sure model gen directories are fully flushed creating nuget packages.

--- BuildNugetPackages.bat
+++ BuildNugetPackages.bat
@@ -4,6 +4,8 @@ set OUT=%BASE%nuget
 rmdir %OUT%
 mkdir %OUT%
 pushd %BASE%
+rd /s /q src\Threax.ModelGen\bin
+rd /s /q src\Threax.ModelGen\obj
 dotnet build -c Release
 pushd %SEARCH%
 FOR /R %SEARCH% %%I in (Release\*.nupkg) DO copy %%I %OUT%
