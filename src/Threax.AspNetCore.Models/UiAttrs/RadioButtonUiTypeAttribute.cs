d405a85b6c2b91f98885ad42a39f6c1f3d8fb964
Andrew Piper <piper.andrew@spcollege.edu>
2018-02-01 15:54:01 -0500

    Added a ui type attribute for radio buttons.

---
+++ src/Threax.AspNetCore.Models/UiAttrs/RadioButtonUiTypeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Threax.AspNetCore.Models
+{
+    /// <summary>
+    /// Use this to change the ui type of a property to a set of radio buttons. Useful if you are going
+    /// to provide values for this property with a value provider and want them displayed
+    /// using radio buttons.
+    /// </summary>
+    public class RadioButtonUiTypeAttribute : UiTypeAttribute
+    {
+        public const String UiName = "radiobutton";
+
+        public RadioButtonUiTypeAttribute() : base(UiName)
+        {
+        }
+    }
+}
