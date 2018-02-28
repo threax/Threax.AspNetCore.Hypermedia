1c46e09152783a522b549d78964471e1604279d5
Andrew Piper <piper.andrew@spcollege.edu>
2018-02-28 14:30:11 -0500

    Made code only output if the entire file is generated successfully.

--- src/Threax.AspNetCore.Halcyon.ClientGen.Tools/ToolsExtensions.cs
+++ src/Threax.AspNetCore.Halcyon.ClientGen.Tools/ToolsExtensions.cs
@@ -10,7 +10,7 @@ namespace Threax.AspNetCore.Halcyon.ClientGen
     {
         public static IToolRunner UseClientGenTools(this IToolRunner toolRunner)
         {
-            return toolRunner.AddTool("clientgen", new ToolCommand("Generate a client library file to the specified destination.", a =>
+            return toolRunner.AddTool("clientgen", new ToolCommand("Generate a client library file to the specified destination.", async a =>
             {
                 if (a.Args.Count < 1)
                 {
@@ -18,27 +18,31 @@ namespace Threax.AspNetCore.Halcyon.ClientGen
                 }
                 var file = a.Args[0];
                 var extension = Path.GetExtension(file);
-                switch (extension)
+                using(var stream = new MemoryStream())
+                using (var writer = new StreamWriter(stream)) //Buffer to a memory stream, this way if there are errors the existing file is not replaced
                 {
-                    case ".ts":
-                        var typescriptWriter = a.Scope.ServiceProvider.GetRequiredService<TypescriptClientWriter>();
-                        using (var writer = new StreamWriter(File.Open(file, FileMode.Create, FileAccess.ReadWrite, FileShare.None)))
-                        {
+                    switch (extension)
+                    {
+                        case ".ts":
+                            var typescriptWriter = a.Scope.ServiceProvider.GetRequiredService<TypescriptClientWriter>();
                             typescriptWriter.CreateClient(writer);
-                        }
-                        break;
-                    case ".cs":
-                        var csharpWriter = a.Scope.ServiceProvider.GetRequiredService<CSharpClientWriter>();
-                        using (var writer = new StreamWriter(File.Open(file, FileMode.Create, FileAccess.ReadWrite, FileShare.None)))
-                        {
+                            break;
+                        case ".cs":
+                            var csharpWriter = a.Scope.ServiceProvider.GetRequiredService<CSharpClientWriter>();
                             csharpWriter.CreateClient(writer);
-                        }
-                        break;
-                    default:
-                        throw new NotSupportedException($"{file} is not supported. Can only generate typescript (.ts) or c# (.cs) clients.");
-                }
+                            break;
+                        default:
+                            throw new NotSupportedException($"{file} is not supported. Can only generate typescript (.ts) or c# (.cs) clients.");
+                    }
 
-                return Task.FromResult(0);
+                    await writer.FlushAsync();
+                    stream.Seek(0, SeekOrigin.Begin);
+
+                    using (var outStream = File.Open(file, FileMode.Create, FileAccess.ReadWrite, FileShare.None))
+                    {
+                        await stream.CopyToAsync(outStream);
+                    }
+                }
             }));
         }
     }
