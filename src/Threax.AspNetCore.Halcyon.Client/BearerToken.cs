963a53b8af1de97d6cfc0b22a138ceeb165b6472
Andrew Piper <piper.andrew@spcollege.edu>
2018-04-12 15:41:50 -0400

    Added a way to handle bearer vs client credentials when requesting entry points.

---
+++ src/Threax.AspNetCore.Halcyon.Client/BearerToken.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threax.AspNetCore.Halcyon.Client
+{
+    /// <summary>
+    /// This class can be injected with an object to make sure the version set up for
+    /// bearer tokens is always returned.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BearerToken<T> : IDisposable
+    {
+        public BearerToken(T wrapped)
+        {
+            this.Wrapped = wrapped;
+        }
+
+        public void Dispose()
+        {
+            (this.Wrapped as IDisposable)?.Dispose();
+        }
+
+        public T Wrapped { get; private set; }
+    }
+}
