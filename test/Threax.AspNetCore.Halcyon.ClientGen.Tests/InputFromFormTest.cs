218f27b9daf908fcf997dfc94135ad8148cc0d0f
Andrew Piper <piper.andrew@spcollege.edu>
2018-07-26 09:19:48 -0400

    Added tests for basic client gen options.

---
+++ test/Threax.AspNetCore.Halcyon.ClientGen.Tests/InputFromFormTest.cs
@@ -0,0 +1,41 @@
+﻿using Halcyon.HAL.Attributes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NJsonSchema.Generation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Threax.AspNetCore.Halcyon.Ext;
+using Threax.AspNetCore.Tests;
+using Xunit;
+
+namespace Threax.AspNetCore.Halcyon.ClientGen.Tests
+{
+    public class InputFromFormTest : FileGenTests<InputFromFormTest.Input, InputFromFormTest.Output>
+    {
+        [HalModel]
+        public class Input
+        {
+            public String Name { get; set; }
+        }
+
+        [HalModel]
+        [HalActionLink(typeof(TestController), nameof(TestController.Save))]
+        public class Output
+        {
+
+        }
+
+        public class TestController : Controller
+        {
+            [HalRel("Save")]
+            [HttpPost]
+            public void Save([FromForm] Input input)
+            {
+
+            }
+        }
+    }
+}
