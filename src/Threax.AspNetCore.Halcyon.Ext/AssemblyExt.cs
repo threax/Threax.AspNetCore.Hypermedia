bc5b0744f28f3c7355ea23470604d59127f8dfeb
Andrew Piper <andrew@threax.com>
2020-07-11 09:29:42 -0400

    Added an assembly hash computing function.

---
+++ src/Threax.AspNetCore.Halcyon.Ext/AssemblyExt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Threax.AspNetCore.Halcyon.Ext
+{
+    public static class AssemblyExt
+    {
+        /// <summary>
+        /// Compute the md5 hash of the target assembly. This will look at the assembly's file and compute the hash.
+        /// </summary>
+        /// <param name="assembly"></param>
+        /// <returns></returns>
+        public static String ComputeMd5(this Assembly assembly)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(assembly.Location))
+                {
+                    return Convert.ToBase64String(md5.ComputeHash(stream));
+                }
+            }
+        }
+    }
+}
