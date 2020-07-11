8afe98b051f5cf7cc14e465d053f95ff67cdb3e3
Andrew Piper <andrew@threax.com>
2020-07-11 10:59:38 -0400

    Return a hex string instead of a base64 string for doc endpoint hashes.

--- src/Threax.AspNetCore.Halcyon.Ext/AssemblyExt.cs
+++ src/Threax.AspNetCore.Halcyon.Ext/AssemblyExt.cs
@@ -11,18 +11,28 @@ namespace Threax.AspNetCore.Halcyon.Ext
     {
         /// <summary>
         /// Compute the md5 hash of the target assembly. This will look at the assembly's file and compute the hash.
+        /// It will be returned as a string of hex characters.
         /// </summary>
         /// <param name="assembly"></param>
         /// <returns></returns>
         public static String ComputeMd5(this Assembly assembly)
         {
+            byte[] hashBytes;
             using (var md5 = MD5.Create())
             {
                 using (var stream = File.OpenRead(assembly.Location))
                 {
-                    return Convert.ToBase64String(md5.ComputeHash(stream));
+                    hashBytes = md5.ComputeHash(stream);
                 }
             }
+
+            var sb = new StringBuilder(hashBytes.Length);
+            foreach (var b in hashBytes)
+            {
+                sb.Append(b.ToString("X2"));
+            }
+
+            return sb.ToString();
         }
     }
 }
