8483c337f48204885b7f2af9fa97f7b2a1e7dd57
Andrew Piper <andrew@threax.com>
2021-02-22 10:03:37 -0500

    Added push file.

---
+++ Push.ps1
@@ -0,0 +1,8 @@
+git push
+git push --tags
+git push https://github.com/threax/Threax.AspNetCore.Hypermedia.git
+git push https://github.com/threax/Threax.AspNetCore.Hypermedia.git --tags
+git push https://gitlab.com/threax-software/Threax.AspNetCore.Hypermedia.git
+git push https://gitlab.com/threax-software/Threax.AspNetCore.Hypermedia.git --tags
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.Hypermedia
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.AspNetCore.Hypermedia --tags
