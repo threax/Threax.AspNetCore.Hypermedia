a2dded4c5376009cc0060f83d632bde03f7a2621
Andrew Piper <andrew@threax.com>
2021-02-22 09:59:13 -0500

    Added time ui type attribute.

---
+++ src/Threax.AspNetCore.Models/UiAttrs/TimeUiTypeAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Threax.AspNetCore.Models
+{
+    public class TimeUiOptions : PropertyUiInfo
+    {
+        public TimeUiOptions(string type) : base(type)
+        {
+        }
+
+        public override string CreateAttribute()
+        {
+            return $@"[TimeUiType({AddSharedProperties(false)})]";
+        }
+    }
+
+    /// <summary>
+    /// Use this to change the ui type of a property to a time. This will
+    /// cause uis to allow just time input instead of date and time.
+    /// </summary>
+    public class TimeUiTypeAttribute : UiTypeAttribute
+    {
+        public const String UiName = "time";
+
+        public TimeUiTypeAttribute() : base(new TimeUiOptions(UiName))
+        {
+        }
+    }
+}
